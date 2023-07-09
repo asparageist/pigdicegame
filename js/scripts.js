a36cfa8d3d95ea12b0fd8b8219e1704cf3f567c2
Joseph Murray <josephwmurray@gmail.com>
2023-07-09 16:56:06 -0700

    fix a few minor bugs with the .hidden class

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,29 @@ window.onload = function () {
   document.getElementById("roll-button").removeAttribute("class", "hidden");
   document.getElementById("hold-button").removeAttribute("class", "hidden");
   document.getElementById("rollem").removeAttribute("class", "hidden");
-  document.getElementById("dang").setAttribute("class", "hidden");
 }
 
 function playDice() {
   let diceValue1 = rollDice();
   let diceValue2 = rollDice();
   let showRoll = document.getElementById("showRoll");
-  showRoll.innerText = "Your Roll:" + diceValue1 + " and " + diceValue2;
+  showRoll.innerText = "Your Roll: " + diceValue1 + " and " + diceValue2;
   let diceImage1 = document.createElement("img");
   let diceImage2 = document.createElement("img");
   let lineBreak = document.createElement("br");
-  diceImage1.src = "img/dice" + diceValue1 + ".png";
-  diceImage2.src = "img/dice" + diceValue2 + ".png";
+  diceImage1.src = "images/dice" + diceValue1 + ".png";
+  diceImage2.src = "images/dice" + diceValue2 + ".png";
   showRoll.appendChild(lineBreak);
   showRoll.appendChild(diceImage1);
   showRoll.appendChild(diceImage2);
 
 
-  if (diceValue1 === 1 || diceValue2 === 1) {
+  if (diceValue1 === 1 && diceValue2 === 1) {
+    document.getElementById("bust").removeAttribute("class", "hidden");
+    currentScore = 0;
+    playerScores[currentPlayer] = 0;
+    changePlayer(currentPlayer, playerScores);
+  } else if (diceValue1 === 1 || diceValue2 === 1) {
     document.getElementById("bust").removeAttribute("class", "hidden");
     currentScore = 0;
     changePlayer(currentPlayer, playerScores);
@@ -41,8 +45,7 @@ function playDice() {
   }                              
 
   if (playerScores[currentPlayer] + currentScore >= 100) {
-    console.log(([currentPlayer] + 1) + " wins");
-    document.getElementById("player" + (currentPlayer + 1)).textContent = playerScores[currentPlayer] + currentScore;
+    document.getElementById("player" + (currentPlayer + 1)).textContent = playerScores[currentPlayer] + currentScore + " WINNER!!";
     endGame();
   }
 }
@@ -61,19 +64,16 @@ function reset() {
   document.getElementById("roll-button").classList.remove("hidden");
   document.getElementById("hold-button").classList.remove("hidden");
   document.getElementById("rollem").classList.remove("hidden");
-  document.getElementById("dang").classList.add("hidden");
 }
 
 
 function changePlayer() {
   playerScores[currentPlayer] += currentScore;
-  if (currentScore === 0) {
-    playerScores[currentPlayer] = 0;
-  }
   document.getElementById("player" + (currentPlayer + 1)).textContent = playerScores[currentPlayer];
   currentScore = 0;
   if (currentPlayer === 0) {
     currentPlayer = 1;
+    document.getElementById("tempScore").classList.add("hidden");
     document.getElementById("player").textContent = currentPlayer + 1;
   } else {
     currentPlayer = 0;
